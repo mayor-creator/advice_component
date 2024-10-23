559466b81a3182afdcc106fcdd9c8dd660f99f0b
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-10-23 12:03:21 -0400

    change generatorAdvice function to handle error message

--- app/index.js
+++ app/index.js
@@ -5,38 +5,37 @@ const adviceMessage = document.getElementById("advice_message");
 const dialog = document.getElementById("dialog");
 const dialog_btn = document.getElementById("dialog_btn");
 
-const generatorAdvice = () => {
+const generatorAdvice = async () => {
 	const id = adviceId.value;
 	const getData = fetch(`https://api.adviceslip.com/advice/${id}`);
-	getData
-		.then((res) => {
-			return res.json();
-		})
-		.then((json) => {
-			const adviceData = json.slip;
-			adviceNumber.innerHTML = `Advice# ${adviceData.id}`;
-			adviceMessage.innerHTML = `"${adviceData.advice}"`;
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+	try {
+		const response = await getData;
+		const json = await response.json();
+		if (response.ok) {
+			if (json.hasOwnProperty("slip")) {
+				const adviceData = json.slip;
+				adviceNumber.innerHTML = `Advice# ${adviceData.id}`;
+				adviceMessage.innerHTML = `"${adviceData.advice}"`;
+			} else if (json.hasOwnProperty("message")) {
+				showDialog();
+				closeDialog();
+			}
+		}
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const showDialog = () => {
 	dialog.showModal();
+	dialog.setAttribute("class", "dialogShow");
 };
 
 const closeDialog = () => {
 	dialog_btn.addEventListener("click", function () {
 		dialog.close();
+		dialog.removeAttribute("class", "dialogShow");
 	});
 };
 
 adviceBtn.addEventListener("click", generatorAdvice);
-
-/* 
-display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-*/
